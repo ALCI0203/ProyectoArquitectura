869fc36daf1f18e807bdc17469f8b820322b54dd
ALCI0203 <alcidesescorza0203@gmail.com>
2018-08-04 22:16:12 -0500

    commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://heroku_l9x5dqp0:f2s9e2ccristqk3e146r7ui2vc@ds213612.mlab.com:13612/heroku_l9x5dqp0');
+//ghjk
 var Employee = mongoose.model('Employee', mongoose.Schema({
 	nombre:String,
 	cedula:String,
