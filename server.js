27053c323d1a4a16a376208ecf0a91633d3170a4
ALCI0203 <alcidesescorza0203@gmail.com>
2018-08-04 21:40:54 -0500

    Proyecto
    Proyecto

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/employees');
+var Employee = mongoose.model('Employee', mongoose.Schema({
+	nombre:String,
+	cedula:String,
+	fecha:Date,
+	estatura:String
+}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/client'));
+
+app.get('/api/employees', function(req, res){
+	Employee.find(function(err, employees){
+		if(err)
+			res.send(err);
+		res.json(employees);
+	});
+});
+
+app.get('/api/employees/:id', function(req, res){
+	Employee.findOne({_id:req.params.id}, function(err, employee){
+		if(err)
+			res.send(err);
+		res.json(employee);
+	});
+});
+app.post('/api/employees', function(req, res){
+	Employee.create( req.body, function(err, employees){
+		if(err)
+			res.send(err);
+		res.json(employees);
+	});
+});
+
+app.delete('/api/employees/:id', function(req, res){
+	Employee.findOneAndRemove({_id:req.params.id}, function(err, employee){
+		if(err)
+			res.send(err);
+		res.json(employee);
+	});
+});
+app.put('/api/employees/:id', function(req, res){
+	var query = {
+		nombre:req.body.nombre,
+		cedula:req.body.cedula,
+		fecha:req.body.fecha,
+		estatura:req.body.estatura
+	};
+	Employee.findOneAndUpdate({_id:req.params.id}, query, function(err, employee){
+		if(err)
+			res.send(err);
+		res.json(employee);
+	});
+});
+app.listen(3000, function(){
+	console.log('server is running on port 3000..');
+});
